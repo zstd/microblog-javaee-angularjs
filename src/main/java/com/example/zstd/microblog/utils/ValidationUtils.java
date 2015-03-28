1fea4962ac63a9ad6db4ea988cd052a9056b33fc
Andrey Zaytsev <azaytsev@aligntech.com>
2015-03-28 17:12:54 +0300

    validation utils deprecation

--- src/main/java/com/example/zstd/microblog/utils/ValidationUtils.java
+++ src/main/java/com/example/zstd/microblog/utils/ValidationUtils.java
@@ -1,6 +1,10 @@
 package com.example.zstd.microblog.utils;
 
 
+/**
+ * Prefer using Google Guava preconditions.
+ */
+@Deprecated
 public class ValidationUtils {
 	
 	public static final void checkArgument(boolean condition,RuntimeException exception) {
