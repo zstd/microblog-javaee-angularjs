2187f33bc0d34cb2aeaf40854c1da84332fd2ee1
Andrey Zaytsev <azaytsev@aligntech.com>
2016-04-28 17:17:27 +0300

    moving to java8

--- src/main/java/com/example/zstd/microblog/service/BlogPostService.java
+++ src/main/java/com/example/zstd/microblog/service/BlogPostService.java
@@ -6,7 +6,6 @@ import com.example.zstd.microblog.model.User;
 import com.example.zstd.microblog.repository.BlogPostRepo;
 import com.example.zstd.microblog.repository.FollowDataRepo;
 import com.example.zstd.microblog.repository.UserRepo;
-import com.example.zstd.microblog.repository.impl.JdbcFollowDataRepo;
 import com.example.zstd.microblog.utils.StringUtils;
 import com.example.zstd.microblog.utils.ValidationUtils;
 
@@ -19,17 +18,20 @@ public class BlogPostService {
 	
 	private static final int DISCOVER_LIST_SIZE = 10;
     public static final String MENTION_PREFIX = "@";
+	public static final String TOPIC_PREFIX = "#";
 
 	private static final Logger LOG = Logger.getLogger(BlogPostService.class.getName());
 	
-	private FollowDataRepo followDataRepo = new JdbcFollowDataRepo();
-
-    private BlogPostRepo blogPostRepo() {
+	private BlogPostRepo blogPostRepo() {
         return ServiceLocator.getInstance().getService(BlogPostRepo.class);
     }
 
 	private UserRepo userRepo() {
 		return ServiceLocator.getInstance().getService(UserRepo.class);
+	}
+
+	private FollowDataRepo followDataRepo() {
+		return ServiceLocator.getInstance().getService(FollowDataRepo.class);
 	}
 	
 	public BlogPost save(String username,String message) {
@@ -51,23 +53,12 @@ public class BlogPostService {
 
         return blogPostRepo().save(toSave);
     }
-	
-	public List<BlogPost> getList(String usernameToExclude) {
-		List<BlogPost> posts;
-        posts = blogPostRepo().listAll();
-        return posts;
 
-    }
-	
 	public List<BlogPost> getListForTopic(String topic) {
 		List<BlogPost> posts = getList();
-		List<BlogPost> result = new ArrayList();
-		for(BlogPost post : posts) {
-			if(post.getTopics() != null && Arrays.asList(post.getTopics().split(",")).contains(topic)) {
-				result.add(post);
-			}
-		}
-		return result;		
+		return posts.stream()
+				.filter(post -> post.getTopics() != null && Arrays.asList(post.getTopics().split(",")).contains(topic))
+				.collect(Collectors.toList());
 	}
 	
 	public List<BlogPost> getListForUser(String user) {
@@ -79,30 +70,21 @@ public class BlogPostService {
 	
 	public List<BlogPost> getListOfOtherUser(String user) {
 		List<BlogPost> posts = getList();
-		List<BlogPost> result = new ArrayList();
-		for(BlogPost post : posts) {
-			if(!post.getCreator().equals(user)) {
-				result.add(post);
-			}
-		}
-		return result;		
+		return posts.stream()
+				.filter(p -> !p.getCreator().equals(user))
+				.collect(Collectors.toList());
 	}
 	
 	public List<BlogPost> getList() {
-		List<BlogPost> posts;
-        posts = blogPostRepo().listAll();
+		List<BlogPost> posts = blogPostRepo().listAll();
         return posts;
-
     }
 
 	private List<String> extractTopics(String[] tokens) {
-		List<String> result = new ArrayList<>();
-		for(String token : tokens) {
-			if(token.startsWith("#")) {
-				result.add(token.replaceAll("#",""));
-			}
-		}
-		return result;
+		return Arrays.stream(tokens)
+				.filter(t -> t.startsWith(TOPIC_PREFIX))
+				.map(t -> t.replaceAll(TOPIC_PREFIX,""))
+				.collect(Collectors.toList());
 	}
 	
 	private List<String> extractMentions(String[] tokens) {
@@ -119,14 +101,12 @@ public class BlogPostService {
 
 	public List<BlogPost> discoverMessagesForUser(String name) {
 		List<BlogPost> data = getListOfOtherUser(name);
-		Map<String, Long> followingData;
-        followingData = userRepo().getUserFollowingData();
+		Map<String, Long> followingData = userRepo().getUserFollowingData();
         Comparator<BlogPost> comparator = BlogPost.createPriorityComparator(followingData);
-		Collections.sort(data,comparator);
-		if(data.size() > DISCOVER_LIST_SIZE) {
-			data = data.subList(0, DISCOVER_LIST_SIZE);
-		}
-		return data;
+		return data.stream()
+				.sorted(comparator)
+				.limit(DISCOVER_LIST_SIZE)
+				.collect(Collectors.toList());
 	}
 
 	public List<BlogPost> getMessagesOfUserFollowers(String name) {
@@ -139,12 +119,11 @@ public class BlogPostService {
 	}
 
 	private List<String> getFollowersNames(String name) {
-		List<String> result = new ArrayList();
-        List<FollowData> followData = followDataRepo.findByField(FollowData.DB_FIELD_FOLLOWING, name);
-        for(FollowData data : followData) {
-            result.add(data.getFollower());
-        }
-        return result;
+		List<FollowData> followData = followDataRepo().findByField(FollowData.DB_FIELD_FOLLOWING, name);
+        return followData.stream()
+				.map(FollowData::getFollower)
+				.collect(Collectors.toList());
+
 	}
 	
 	
